31a7ebf2e68bb7fe5bf32142338feb98793a75d4
Davy Woolley <davywoolley@gmail.com>
2023-02-17 15:26:43 -0300

    TerceiraAtvd

---
+++ TerceiraAtvd/TerceiraAtvd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace TerceiraAtvd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu Número de Identificação");
+            int NumFunc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Por quantas horas você trabalhou?");
+            double HorasTrab = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O Quanto você recebe por hora trabalhada?");
+            double ValorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = HorasTrab * ValorHora;
+
+            Console.WriteLine("Number: " + NumFunc);
+            Console.WriteLine("Salary: " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
