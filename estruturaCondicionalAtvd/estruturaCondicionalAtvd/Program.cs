185be2f32f426662851005914d332ff9c13e0980
Davy Woolley <davywoolley@gmail.com>
2023-02-28 19:34:31 -0300

    Negativo ou Positivo

---
+++ estruturaCondicionalAtvd/estruturaCondicionalAtvd/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um valor: ");
+int v1 = int.Parse(Console.ReadLine());
+
+if(v1 >= 0)
+{
+    Console.WriteLine("O número é Positivo!");
+} else
+{
+    Console.WriteLine("O número é Negativo!");
+}
