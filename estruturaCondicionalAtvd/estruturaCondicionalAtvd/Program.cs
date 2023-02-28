cbb961e0c568c30afbc8e5693afd90f0306519a9
Davy Woolley <davywoolley@gmail.com>
2023-02-28 20:07:07 -0300

    Cardapio Lanchonete

--- estruturaCondicionalAtvd/estruturaCondicionalAtvd/Program.cs
+++ estruturaCondicionalAtvd/estruturaCondicionalAtvd/Program.cs
@@ -1,18 +1,41 @@
-﻿Console.Write("Digite o horario de inicio: ");
-int horaInicial = int.Parse(Console.ReadLine());
+﻿using System.Globalization;
 
-Console.Write("Digite o horario de termino: ");
-int horaFinal = int.Parse(Console.ReadLine());
+Console.WriteLine(" CÓDIGO  |  ESPECIFICAÇÃO  | PREÇO");
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("   1     Cachorro quente   R$ 4.00");
+Console.WriteLine("   2     X-Salada          R$ 4.50");
+Console.WriteLine("   3     X-Bacon           R$ 5.00");
+Console.WriteLine("   4     Torrada Simples   R$ 2.00");
+Console.WriteLine("   5     Refrigerante      R$ 1.50");
+Console.WriteLine("-----------------------------------");
+Console.WriteLine(" ");
+Console.Write("Digite o código do pedido: ");
+int codigo = int.Parse(Console.ReadLine());
+Console.Write("Digite a Quantidade: ");
+int quantidade = int.Parse(Console.ReadLine());
 
-int duracao;
+double valor;
+double total;
 
-if (horaInicial < horaFinal)
+if (codigo == 1)
 {
-    duracao = horaFinal - horaInicial;
+    total = quantidade * 4.0;
+}
+else if (codigo == 2)
+{
+    total = quantidade * 4.5;
+}
+else if (codigo == 3)
+{
+    total = quantidade * 5.0;
+}
+else if (codigo == 4)
+{
+    total = quantidade * 2.0;
 }
 else
 {
-    duracao = 24 - horaInicial + horaFinal;
+    total = quantidade * 1.5;
 }
 
-Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
