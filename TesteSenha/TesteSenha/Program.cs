36ddc8da362084648a4e41ebc7c8502dd8e7a100
Davy Woolley <davywoolley@gmail.com>
2023-03-07 20:51:28 -0300

    TesteSenha

---
+++ TesteSenha/TesteSenha/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Digite sua Senha: ");
+int senha = int.Parse(Console.ReadLine());
+
+while (senha != 2002)
+{
+    Console.WriteLine("Senha Invalida");
+    senha = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Acesso Permitido");
