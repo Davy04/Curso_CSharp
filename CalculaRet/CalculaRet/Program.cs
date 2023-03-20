9fd3763b8e7d209a6dadf37da09681e9631c273a
Davy Woolley <davywoolley@gmail.com>
2023-03-20 20:22:02 -0300

    CalculaArea

---
+++ CalculaRet/CalculaRet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculaRet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retangulo: ");
+            Console.Write("Largura: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Altura: ");
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area: " + ret.CalculaArea().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + ret.CalculaPerimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + ret.CalculaDiagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ CalculaRet/CalculaRet/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaRet
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+        public double Area;
+        public double Perimetro;
+        public double Diagonal;
+
+
+        public double CalculaArea()
+        {
+            Area = Largura * Altura;
+            return Area;
+        }
+
+        public double CalculaPerimetro()
+        {
+            Perimetro = Altura * 2 + Largura * 2;
+            return Perimetro;
+        }
+
+        public double CalculaDiagonal()
+        {
+            Diagonal = Math.Sqrt(Math.Pow(Largura, 2.0) + Math.Pow(Altura, 2.0));
+            return Diagonal;
+        }
+    }
+}
